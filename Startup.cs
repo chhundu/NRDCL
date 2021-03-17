eedf0c501639bb5f53282e13fad9823f99d7fa44
Chhundu Dorji <chundudorji2018@gmail.com>
2021-03-17 16:14:31 +0600

    added authenticatio

--- Startup.cs
+++ Startup.cs
@@ -51,12 +51,13 @@ namespace NRDCL
 
             app.UseRouting();
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Login}/{id?}");
             });
         }
     }
