71b75aacbd69bc289a6a7e46b679a88075ce9bd6
Manta Kumari Laksam <mantalaksamba@gmail.com>
2021-03-22 12:57:07 +0600

    ÄddedAccountController

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NRDCL.IService.User;
 using NRDCL.Models.Acc;
+using NRDCL.Models.Common;
 using NRDCL.Repository;
 using System;
 using System.Collections.Generic;
@@ -10,46 +14,63 @@ namespace NRDCL.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly IAccountRepository accountRepository;
+        private readonly IAccountRepository _accountRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserService _userService;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(IAccountRepository accountRepository)
+        public AccountController(IAccountRepository accountRepository, UserManager<ApplicationUser> userManager, IUserService userService, RoleManager<IdentityRole> roleManager)
         {
-            this.accountRepository = accountRepository;
+            _userService = userService;
+            _roleManager = roleManager;
+            _accountRepository = accountRepository;
+            _userManager = userManager;
         }
         public IActionResult Signup()
         {
+            var roles = _roleManager.Roles;
+            ViewData["Roles"] = new SelectList(roles);
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> Signup(SignupUserModel userModel)
         {
+            var roles = _roleManager.Roles;
+            ViewData["Roles"] = new SelectList(roles);
             if (ModelState.IsValid)
             {
-                var result = await this.accountRepository.CreateUserAsync(userModel);
+                var result = await _accountRepository.CreateUserAsync(userModel);
+      
                 if (!result.Succeeded)
                 {
-                    foreach(var errorMsg in result.Errors)
+                    foreach (var errorMsg in result.Errors)
                     {
                         ModelState.AddModelError("", errorMsg.Description);
                     }
                     return View(userModel);
                 }
+                ViewBag.Result = CommonProperties.signupSuccessMsg;
                 ModelState.Clear();
+                userModel = new SignupUserModel();
             }
-            return View();
+            return View(userModel);
         }
 
         public IActionResult Signin()
         {
+            var roles = _roleManager.Roles;
+            ViewData["Roles"] = new SelectList(roles);
             return View();
         }
         [HttpPost]
         public async Task<IActionResult> Signin(SigninUserModel signinUserModel, string returnUrl)
         {
+            var roles = _roleManager.Roles;
+            ViewData["Roles"] = new SelectList(roles);
             if (ModelState.IsValid)
             {
-                var result = await this.accountRepository.PasswordSignInAsync(signinUserModel);
+                var result = await _accountRepository.PasswordSignInAsync(signinUserModel);
                 if (result.Succeeded)
                 {
                     if (!string.IsNullOrEmpty(returnUrl))
@@ -65,8 +86,8 @@ namespace NRDCL.Controllers
 
         public async Task<IActionResult> Logout()
         {
-            await this.accountRepository.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            await _accountRepository.SignOutAsync();
+            return RedirectToAction("Signin");
         }
         [Route("change-password")]
         public IActionResult ChangePassword()
@@ -74,11 +95,21 @@ namespace NRDCL.Controllers
             return View();
         }
         [HttpPost("change-password")]
-        public IActionResult ChangePassword(ChangePasswordModel changePasswordModel)
+        public async Task<IActionResult> ChangePassword(ChangePasswordModel changePasswordModel)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-
+                var result = await _accountRepository.ChangePasswordAsync(changePasswordModel);
+                if (result.Succeeded)
+                {
+                    ViewBag.IsSuccess = CommonProperties.updateSuccessMsg;
+                    ModelState.Clear();
+                    return View();
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
             }
             return View(changePasswordModel);
         }
