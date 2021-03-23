939def1be062d76f12ade24e4089b8abb4f4ab4d
Manta Kumari Laksam <mantalaksamba@gmail.com>
2021-03-23 14:17:09 +0600

    AddedUserRole&EditModels

---
+++ Models/Acc/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NRDCL.Models.Acc
+{
+    public class UserRole
+    {
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
