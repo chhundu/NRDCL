770b74706c65cc86472602cbf4e25540c3d6a4ee
Chhundu Dorji <chundudorji2018@gmail.com>
2021-03-24 14:44:27 +0600

    Adeed access denied page

---
+++ Controllers/AccessDeniedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NRDCL.Controllers
+{
+    public class AccessDeniedController : Controller
+    {
+        public IActionResult AccessDenied403()
+        {
+            return View();
+        }
+    }
+}
