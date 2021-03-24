902a094def36df2837766eb481dfdd9c46b10984
Manta Kumari Laksam <mantalaksamba@gmail.com>
2021-03-24 12:50:57 +0600

    AddedAuthorizationToFilesThatCanBeAccessedByAdminOnly

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace NRDCL.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
